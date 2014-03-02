8e3ba9482f37c97b70a41a00383c15e387508050
Rudi Grinberg <rudi.grinberg@gmail.com>
2014-03-01 20:06:38 -0800

    reindent with ocp

--- src/ackdo.ml
+++ src/ackdo.ml
@@ -10,25 +10,25 @@ let command =
     Command.Spec.(
       empty
       +> flag "-d" (no_arg)
-          ~doc:" Write changes. Mnemonic: d => do"
+           ~doc:" Write changes. Mnemonic: d => do"
       +> flag "-w" (optional_with_default (Unix.getcwd ()) string)
-          ~doc:"directory Set the directory to lookup the files in the change
+           ~doc:"directory Set the directory to lookup the files in the change
           set The default is cwd"
       +> flag "-c" (no_arg)
-          ~doc:" Turn off color output"
+           ~doc:" Turn off color output"
     ) (fun commit dir color () -> 
-        let open Ackdo_lib in
-        try begin
-          let input = In_channel.(input_lines stdin) in
-          let change_sets = Ackdo_lib.parse_changes ~dir input in
-          if commit then Ackdo_lib.write_changes change_sets
-          else Ackdo_lib.preview_changes change_sets ~color:(not color)
+      let open Ackdo_lib in
+      try begin
+        let input = In_channel.(input_lines stdin) in
+        let change_sets = Ackdo_lib.parse_changes ~dir input in
+        if commit then Ackdo_lib.write_changes change_sets
+        else Ackdo_lib.preview_changes change_sets ~color:(not color)
+      end
+      with _exn -> begin
+          match _exn with
+          | Unimplemented msg -> printf "%s\n" msg
+          | _exn -> E.handle_exn _exn
         end
-        with _exn -> begin
-            match _exn with
-            | Unimplemented msg -> printf "%s\n" msg
-            | _exn -> E.handle_exn _exn
-          end
-      )
+    )
 
 let () = Exn.handle_uncaught ~exit:true (fun () -> Command.run command)
